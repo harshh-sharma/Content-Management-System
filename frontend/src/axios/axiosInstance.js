cef3f04c756dd06117357df1405ded166f82c3b8
Harsh Sharma <124427013+harshh-sharma@users.noreply.github.com>
2025-01-13 15:48:25 +0530

    set up axios instance

---
+++ frontend/src/axios/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BaseUrl = 'http://localhost:3000/api/v1';
+
+// Create an axios instance
+const axiosInstance = axios.create({
+  baseURL: BaseUrl, 
+  withCredentials: true
+});
+
+export default axiosInstance;
